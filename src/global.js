dbb3b37fc69c991e9341c81b50a2d67ad6931cc3
Chrigor <chrigor.edu3@gmail.com>
2020-05-28 14:54:50 -0300

    Estrutura inicial do projeto

---
+++ src/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+
+const GlobalStyle = createGlobalStyle`
+
+  html, body {
+    margin:0;
+    padding:0;
+    background-color:#18191A;
+  }
+
+  button {
+    border: none;
+  }
+
+`;
+
+export default GlobalStyle;
